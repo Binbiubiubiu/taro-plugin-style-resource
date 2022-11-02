00f9a2fa713bd66665f3419472bb45368d17c684
Binbiubiubiu <2632003786@qq.com>
2022-11-03 02:57:31 +0800

    build: 修改e2e脚本

--- examples/taro-stylus-project/scripts/test.mjs
+++ examples/taro-stylus-project/scripts/test.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env zx
 
 const PORT = 8080
-await $`npm run build:h5`
-let server = $`live-server dist --port ${PORT} --no-browser --quiet`
+await $`npm run build:h5`.quiet()
+let server = $`live-server dist --port ${PORT} --no-browser --quiet`.quiet()
 server.catch(()=>{})
 await $`wait-on http://localhost:${PORT} && npm run cy:run`
-await $`kill-port ${PORT}`
+await server.kill();
